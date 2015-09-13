e80d6c7a5afc8e9a86dfdd89e9020f9b39ff46e9
Chris Fetherston <chris@cfetherston.com>
2015-09-12 20:30:50 -0400

    stuff bouncing around screen

--- app/scripts/models/particle-model.js
+++ app/scripts/models/particle-model.js
@@ -2,6 +2,9 @@
 
 var Backbone = require('Backbone');
 
+var X_BOUNDS_COLLISION = 'Collided with world sides';
+var Y_BOUNDS_COLLISION = 'Collided with world top or bottom';
+
 var ParticleModel = Backbone.Model.extend({
 
     defaults: {
@@ -17,6 +20,7 @@ var ParticleModel = Backbone.Model.extend({
         this.worldModel = options.worldModel;
         this.setRandom();
         this.listenTo(this.worldModel, 'tick', this.setPosition);
+        this.listenTo(this, 'invalid', this.onValidationError);
     },
 
     setRandom: function() {
@@ -33,13 +37,45 @@ var ParticleModel = Backbone.Model.extend({
         var angle = this.angleAsRadians();
         var currX = this.get('x');
         var currY = this.get('y');
-        this.set('x', currX + (v * Math.cos(angle)));
-        this.set('y', currY + (v * Math.sin(angle)));
+
+        this.set({
+            'x': currX + (v * Math.cos(angle)),
+            'y': currY + (v * Math.sin(angle))
+        }, {validate: true});
         this.trigger('positionChange');
     },
 
     angleAsRadians: function() {
         return this.get('angle') * (Math.PI / 180);
+    },
+
+    validate: function(attrs) {
+        var x = attrs.x || false;
+        var y = attrs.y || false;
+        // world bounds collision validation
+        if (x || y) {
+            var worldHeight = this.worldModel.get('height');
+            var worldWidth = this.worldModel.get('width');
+            if (x <= 0 || x >= worldWidth) {
+                return X_BOUNDS_COLLISION;
+            }
+
+            if (y <= 0 || y >= worldHeight) {
+                return Y_BOUNDS_COLLISION;
+            }
+        }
+    },
+
+    onValidationError: function(model, error) {
+        if (error === X_BOUNDS_COLLISION) {
+            console.log('OUT OF X BOUNDS', this.get('angle'));
+            this.set('angle', 180 - this.get('angle'));
+        }
+        if (error === Y_BOUNDS_COLLISION) {
+            console.log('OUT OF Y BOUNDS', this.get('angle'));
+            this.set('angle', -this.get('angle'));
+        }
+
     }
 });
 
