112dff407bdaafcf1ea231d79dd029baca1e57d2
Chris Fetherston <chris@cfetherston.com>
2015-10-05 21:04:34 -0400

    fuck bower

--- gulp/config.js
+++ gulp/config.js
@@ -2,7 +2,6 @@ var path = require('path');
 var production = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-  bower: 'app/bower_components',
   dist: production ? 'dist' : '.tmp',
 	livereloadPort: 35729,
 	port: 9000,
